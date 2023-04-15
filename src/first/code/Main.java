c6d193affd8743160132e45221848a9ab700389c
Arielqqq <ariel.wu.94@gmail.com>
2023-04-15 17:10:42 +1000

    Arrays

---
+++ src/first/code/Main.java
@@ -0,0 +1,11 @@
+package first.code;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args){
+        int[] numbers = {2, 3, 4, 5, 3};
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+}
